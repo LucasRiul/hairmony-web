be11c31ca97b5de0c353d1993db1d3cabb82b6d3
Lucas Riul Martins <lucasriul85@icloud.com>
2025-04-23 01:17:21 -0300

    ajustes navbar

--- hairmony-web/src/app/modules/agenda/agenda.ts
+++ hairmony-web/src/app/modules/agenda/agenda.ts
@@ -1,3 +1,12 @@
-export  agenda = {
-    
+export class agenda {
+    id?: string;
+    data_de?: string;
+    data_ate?: string;
+    concluido?: string;
+    data_criacao?: string;
+    salaoId?: string;
+    clienteId?: string;
+    servicoId?: string;
+    colaboradorId?: string;
+
 }
