790a1b8a24e6518c9e84630d08ddebc718410758
Lucas Riul Martins <lucasriul85@icloud.com>
2025-05-21 03:07:29 -0300

    environment

--- hairmony-web/src/environments/environment.development.ts
+++ hairmony-web/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7199/api'
+    // apiUrl: 'https://localhost:7199/api'
+  apiUrl: 'https://hairmony-api-production.up.railway.app/api'
+
 };
