efc1b2907bca8d25341a8db60780ecf8fae1c609
lucas.riul <lucas.riul@treeinova.com.br>
2025-05-22 09:02:44 -0300

    enrironment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-  apiUrl: 'https://localhost:7199/api'
-  // apiUrl: 'https://hairmony-api-production.up.railway.app/api'
+  // apiUrl: 'https://localhost:7199/api'
+  apiUrl: 'https://hairmony-api-production.up.railway.app/api'
 };
